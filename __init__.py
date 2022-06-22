d9921086157b1a301631c87f10f4a6521a07365e
Theo Bird <81536891+theobird-MM@users.noreply.github.com>
2022-06-22 14:04:07 +1000

    Secondary Commit

---
+++ __init__.py
@@ -0,0 +1,17 @@
+## Python Boolean Tests
+print("Boolean Testing Commencing...")
+
+a = 530
+b = 75
+c = 50
+
+if a < b and a != c:
+    print("Input is too smol.")
+elif a == c:
+    print("Input's the same as C.")
+else:
+    print(a,"is good.\nYou may continue...\n...")
+
+print("Which way do you want to go?")
+
+
